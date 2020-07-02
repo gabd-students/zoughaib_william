293ac8c5442c9fef9fd9ab4da067db263dcdf34f
William Zoughaib <jane@example.com>
2020-07-02 09:14:13 -0500

    initial commit

---
+++ hw02/hw02-1.R
@@ -0,0 +1,27 @@
+# William Zoughaib
+# HW02 Part 1: Intro to Basics
+# Hello, My name is Joe
+# I work in a button factory
+# One day my boss came up to me
+# Calculate 3 + 10
+ 3 + 10 
+3 + 10 
+# 30 cents plus 12 cents means 42 cents in total.  
+30 + 12 
+
+# Section 1.2
+# Division
+121/1.1 
+# Exponentiation
+2^5
+# Modulo
+28%%6
+#Exponentiation without parantheses
+49^1/2
+#Exponentiation with parantheses
+49^(1/2)
+# Section 1.3 Variable Assignment 42
+# Assign 42 to x 
+x <- 42
+# Print out x#
+
